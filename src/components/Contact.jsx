7bf01e9bad477d9b6eb47756a8f89912015697f1
TsilaAlloui <tsila.allaoui@gmail.com>
2023-02-22 10:01:17 +0300

    Added Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,26 @@
 import "../css/Contact.scss";
+import {AiFillFacebook, AiFillGithub, AiFillLinkedin} from "react-icons/ai"
 
 function Contact(){
     return <div className="contact">
-
+        <div className="social">
+            <AiFillGithub id="github"/>
+            <div className="linker">
+                <div className="dot"></div>
+                <div className="line"></div>
+                <div className="dot"></div>
+            </div>
+            <AiFillFacebook id="fb"/>
+            <div className="linker-right">
+                <div className="dot"></div>
+                <div className="line"></div>
+                <div className="dot"></div>
+            </div>
+            <AiFillLinkedin id="linkedin"/>
+        </div>
+        <div className="copyright">
+            <p>TsilaAlloui - 2023</p>
+        </div>
     </div>;
 }
 
