0c9c6481ec783819b70abd89dbf5f63e5123664d
TahiryAllaoui <tallaoui31@gmail.com>
2023-07-03 21:03:14 +0300

    renew

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-import "../css/App.scss";
-import Navbar from "./Navbar";
-import About from "./About";
-import Projects from "./Projects";
-import Contact from "./Contact";
+import "../styles/App.scss";
+import KUTE from "kute.js";
 
 function App() {
+
+  // const tween = KUTE.fromTo(
+  //   "#layer1",
+  //   { path: "#layer1" },
+  //   { path: "#layer2" },
+  //   { repeat: 999, duration: 3000, yoyo: true }
+  // );
+
+  // useEffect(() => {
+  //   tween.start();
+  // }, [])
+
+
   return <div className="app">
-    <Navbar/>
-    <About/>
-    <Projects/>
-    <Contact/>
-    </div>;
+    <div className="spacer layer1"></div>
+    <div className="spacer layer2"></div>
+  </div>;
 }
 
 export default App;
