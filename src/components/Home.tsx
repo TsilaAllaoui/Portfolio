f8141c9bd147df428251646241ec1978b3dd14fa
TsilaAlloui <tsila.allaoui@gmail.com>
2023-07-11 16:18:08 +0300

    Added circles and animation for Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,9 @@ import "../styles/Home.scss";
 function Home() {
   return (
     <div id="home">
+      <div id="first-circle"></div>
+      <div id="second-circle"></div>
+      <div id="third-circle"></div>
       <Bio />
       <Waves />
     </div>
